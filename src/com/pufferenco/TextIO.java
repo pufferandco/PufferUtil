faf728d6f933b71bd8d94a051876d763f51a1056
pufferandco <sanderalberts11@gmail.com>
2023-01-29 20:04:41 +0100

    goodafternoon

--- src/com/pufferenco/TextIO.java
+++ src/com/pufferenco/TextIO.java
@@ -2,13 +2,25 @@ package com.pufferenco;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 
 public class TextIO {
-   public static void WriteTxt(String file, String content){
-      try(FileWriter myWriter = new FileWriter(file)) {
+   public static void WriteTxt(String file, String content) {
+      try (FileWriter myWriter = new FileWriter(file)) {
          myWriter.write(content);
       } catch (IOException e) {
          e.printStackTrace();
+      }
+   }
+
+   public static void AppendTxt(String file, String line) {
+      try {
+         Files.write(Paths.get(file), line.getBytes(), StandardOpenOption.APPEND);
+      } catch (IOException e) {
+         //exception handling left as an exercise for the reader
       }
    }
 }
--- src/com/pufferenco/async/ThreadManager.java
+++ src/com/pufferenco/async/ThreadManager.java
@@ -1,5 +1,6 @@
 package com.pufferenco.async;
 
+import java.util.LinkedList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.LinkedBlockingDeque;
 
@@ -10,6 +11,7 @@ public class ThreadManager {
    private final Object ThreadMonitor = new Object();
    private final threader[] threads;
    private final LinkedBlockingDeque<Promise> Tasks = new LinkedBlockingDeque<>();
+   private final LinkedList<Promise<?>> ids = new LinkedList<>();
 
    /**
     * creates a thread-manager for easy code snippets
@@ -62,12 +64,26 @@ public class ThreadManager {
       return task;
    }
 
+   /**
+    * runs execute but with an id which you can put in the ThreadManager.get(int)
+    * method to get the Promise without needing a variable
+    *
+    * @param function the function to use
+    * @param id       the id to use
+    */
+   public <V> void exec(Callable<V> function, int id) {
+      ids.add(id, exec(function));
+   }
+
+   public <V> Promise<V> get(int id) {
+      return (Promise<V>) ids.get(id);
+   }
 
    /**
     * Finishes the queue
     * and then close all threads.
     */
-   public void Finish(){
+   public void Finish() {
       for (threader thread : threads) {
          thread.Finish = true;
       }
@@ -84,22 +100,21 @@ public class ThreadManager {
       public void run() {
          while (true) {
             try {
-               @SuppressWarnings("Raw Types")
                Promise e = Tasks.pollFirst();
 
-               if(e == null) {
-                  if(Finish)
-                     break;
-
-                  synchronized (ThreadMonitor) {
-                     ThreadMonitor.wait();
-                     continue;
-                  }
+               if (e != null) {
+                  e.Complete(e.task.call());
+                  continue;
                }
-               Object output = e.task.call();
-               e.Complete(output);
 
-               }catch (Exception exception) {throw new RuntimeException(exception);}
+               if (Finish)
+                  break;
+
+               synchronized (ThreadMonitor) {
+                  ThreadMonitor.wait();
+               }
+
+            }catch (Exception exception) {throw new RuntimeException(exception);}
             }
          }
       }
